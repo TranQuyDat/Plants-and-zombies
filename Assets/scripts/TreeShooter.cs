b9561e574c37c843f4c2db1374bbe11a58712448
TranQuyDat <datraumuong@gmail.com>
2023-12-05 03:08:18 +0700

    05/12/2023 update : file TreeShoter

---
+++ Assets/scripts/TreeShooter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class TreeShooter : MonoBehaviour
+{
+    public float HP;
+    public float timeDelayAct;
+    public GameObject projectile;
+    public Transform posshoot;
+    private void Start()
+    {
+        InvokeRepeating("ActionOfTree", timeDelayAct, timeDelayAct);
+    }
+    public void ActionOfTree() 
+    {
+        Instantiate(projectile, posshoot.position, Quaternion.identity);
+    }
+}
