02f224c52b108c77e64d249f35b5ab2597ab3de8
TranQuyDat <datraumuong@gmail.com>
2023-12-20 00:30:32 +0700

    20/12/2023 update : file pointManager

--- Assets/scripts/SelectedCusor.cs
+++ Assets/scripts/SelectedCusor.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SelectedCusor : MonoBehaviour
 {
     
-    public GameObject slotTree = null;
-    public GameObject cur_tree = null;
-    public GameObject cur_Item = null;
+    public GameObject slotTree ;// slot de chua anh cua tree
+    public GameObject cur_tree = null;// chua prefab cua trees
+    public GameObject cur_Item = null;// chua prefab item (shovel)
+
+    public ShovelBoxController shovelboxctrl;
     [HideInInspector] public bool isSelected ;
     private void Start()
     {
@@ -17,6 +19,7 @@ public class SelectedCusor : MonoBehaviour
     {
         selectTree();
         selectItem();
+        ResetCursor();
     }
 
     public void selectTree()
@@ -37,7 +40,6 @@ public class SelectedCusor : MonoBehaviour
         }
         Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         slotTree.transform.position = new Vector2( pos.x,pos.y-1.5f);
-        cur_Item.transform.position = new Vector2(pos.x, pos.y);
 
     }
     public void selectItem()
@@ -46,5 +48,15 @@ public class SelectedCusor : MonoBehaviour
         Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         cur_Item.transform.position = new Vector2(pos.x, pos.y);
 
+    }
+
+    public void ResetCursor()
+    {
+        if (!Input.GetKeyDown(KeyCode.Mouse1)) return; 
+        cur_tree = null;
+        cur_Item = null;
+        isSelected = false;
+        slotTree.SetActive(false);
+        shovelboxctrl.shovelDropToBox();
     }
 }
---
+++ Assets/scripts/Sun.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    public float timedelay;
+    public int sunPoint;
+    public GameObject target;
+    GameManager gameManager;
+    public bool isaddpoint;
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        target = GameObject.FindGameObjectWithTag("posSun");
+    }
+    private void Update()
+    {
+        StartCoroutine(movetoAndaddpoint());
+    }
+    public void addpoint()
+    {
+        gameManager.pointsManager.addPoint(sunPoint);
+        Destroy(this.gameObject);
+        isaddpoint = false;
+    }
+
+    IEnumerator movetoAndaddpoint()
+    {
+        yield return new WaitForSeconds(5f);
+        MoveTo();
+        yield return new WaitUntil(()=> isaddpoint);
+        addpoint();
+    }
+    public void MoveTo()
+    {
+        Vector3 pos = target.transform.position;
+        this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        this.transform.position = Vector2.Lerp(this.transform.position, pos, 0.1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("posSun"))
+        {
+            isaddpoint = true;
+        }
+    }
+}
