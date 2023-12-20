f4cdc9e2abe4d7de22989b9ef673820033e0d562
Đạt Vũ <datvuhp2002@gmail.com>
2023-12-20 20:53:51 +0700

    Tao animation cho zombie va zombie di chuyen

---
+++ Assets/scripts/Zombie/Zombie.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : ActionOfzombie
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(speed, 0, 0);
+    }
+    public override void action()
+    {
+    }  
+}
