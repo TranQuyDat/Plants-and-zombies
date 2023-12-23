85321547f8e5a725e0388083597bb236174aa44b
TranQuyDat <datraumuong@gmail.com>
2023-12-24 03:48:47 +0700

    24/12/2023 update fix

--- Assets/scripts/Propgress/progressBar.cs
+++ Assets/scripts/Propgress/progressBar.cs
@@ -7,7 +7,12 @@ public class progressBar : MonoBehaviour
 {
     public Slider fillbar;
     public float speed; 
+    public GameObject wavetrigger;
+    public Transform trigger;
+    public SpawnEnemiManager SpawnEnemi;
+    public int curWavecount;
 
+    public List<GameObject> listtriggeractive;
     private void Start()
     {
         fillbar.value = 0;
@@ -15,7 +20,29 @@ public class progressBar : MonoBehaviour
     private void Update()
     {
         fillbar.value = fillbar.value + 0.01f*speed *Time.deltaTime;
-
+        if(curWavecount!= SpawnEnemi.getWavecount())
+        {
+            curWavecount = SpawnEnemi.getWavecount();
+            createWavetrigger();
+        }
+    }
+    public void createWavetrigger()
+    {
+        DestroyAlltriggerWave();
+        for (int i = 0; i < curWavecount; i++)
+        {
+            GameObject trig = Instantiate(wavetrigger, trigger);
+            listtriggeractive.Add(trig);
+        }
+    }
+    
+    public void DestroyAlltriggerWave()
+    {
+        foreach(GameObject trigger in listtriggeractive)
+        {
+            Destroy(trigger);
+        }
+        listtriggeractive.Clear();
     }
 
 }
